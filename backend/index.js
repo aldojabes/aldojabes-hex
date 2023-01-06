11c3f46f8830abe71dd82c84934048313cf4373b
aldojabes <aldojabes@gmail.com>
2023-01-06 16:13:10 +0200

    thats it

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
 import applicationroutes from "./routes/applicationRoutes.js";
 import cors from "cors";
- 
+
+
+const PORT = process.env.PORT || 5000;
 const app = express();
  
 app.use(cors());
 app.use(express.json());
 app.use('/calculate', applicationroutes);
  
-app.listen(5000, () => console.log('Server running at port 5000'));
+app.listen(PORT, () => console.log("Server running at port "+PORT));
 
 export default { app };
